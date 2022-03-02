d5a8836a82adcc9aa3774db8e57cde5c1c303fdc
“Bittoo <abittooa@amazon.com>
2022-03-02 20:51:51 +0530

    class 81

---
+++ class 81/relativeSort.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+
+ var relativeSortArray = function(arr1, arr2) {
+    let map = {};
+    
+    function compare(a,b){
+        //4 9
+        // 4 will come first because index of 4 in arr2 is less
+        // a and b both are present in map
+        if(map[a] !== undefined && map[b] !== undefined){
+            return map[a] - map[b];   
+        }
+
+        // 4 7
+        // a is in arr2  but not b
+        if(map[a] !== undefined && map[b] === undefined){
+            return -1;
+        }
+
+
+        // 7 9
+        // a is not in arr2 but b is
+        if(map[a] === undefined && map[b] !== undefined){
+            return 1;
+        }
+
+
+        //7 19
+        // both are not in arr2
+        return a - b;
+
+
+    }
+
+
+    for(let i = 0;i<arr2.length;i++){
+        map[arr2[i]] = i;
+    }
+    arr1.sort(compare);
+    return arr1;
+    
+};
