4be73a78c6734a604119022bf2c69938eaf259f7
Bittoo Aggarwal <bitz1119@gmail.com>
2022-03-19 13:25:45 +0530

    class 88

---
+++ class 88/linkedlist.js
@@ -0,0 +1,88 @@
+
+class Node {
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+    }
+    addnode(data){
+        let node1 = new Node(data);
+        if(this.head == null){
+            this.head = node1;
+        }
+        else{
+            let temp = this.head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = node1;
+        }
+    }
+    display(){
+        if(this.head == null){
+            console.log("No nodes are available")
+        }
+        else{
+            let temp = this.head;
+            while(temp != null){
+                console.log(temp.data + " --> ");
+                temp = temp.next;
+            }
+        }
+    }
+    // addNodeStart
+    addNodeStart(data){
+        let newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    //addNodeOnIndex -> data, index
+    addNodeOnIndex(data,index){
+        if(index == 0){
+            addNodeStart(data);
+        }
+        else{
+            let currIndex = 0;
+            let temp = this.head;
+            while(currIndex < index-1){
+                temp = temp.next;
+                currIndex++;
+            }
+            let newNode = new Node(data);
+            let next = temp.next;
+            temp.next = newNode;
+            newNode.next = next;
+        }
+    }
+
+}
+
+
+
+
+
+
+let linkedList = new LinkedList();
+linkedList.addnode(1);
+linkedList.addnode(2);
+linkedList.addnode(3);
+linkedList.addnode(4);
+
+linkedList.display();
+
+console.log("================================================")
+linkedList.addNodeStart(0);
+linkedList.display();
+
+console.log("================================================")
+linkedList.addNodeOnIndex(2.5,3);
+linkedList.addNodeOnIndex(3.5,5);
+linkedList.display();
+
