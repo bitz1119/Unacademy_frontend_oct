8aa49efbe3310d7c60d6e9041da8529a5ab19c2c
Bittoo Aggarwal <bitz1119@gmail.com>
2022-03-23 22:58:33 +0530

    stack

--- class 89/Stack.js
+++ class 89/Stack.js
@@ -20,7 +20,7 @@ class Stack{
             return ele;
         }
         //if no element to pop return null
-        return null;
+        throw new Error("stack is empty");
     }
 
     isEmpty() {
@@ -44,10 +44,69 @@ stack.push(100)
 stack.push(200)
 stack.push(300)
 stack.display();
-stack.pop();
-stack.display();
-console.log(stack.isEmpty())
-console.log(stack.getTop())
+
+
+let eleToBePushedAtBottom = 400;
+// pop all the elements from stack to tempStack
+const tempStack = new Stack();
+while(stack.isEmpty()==false){
+    let ele = stack.pop();
+    tempStack.push(ele);
+}
+
+
+// push all the elements from tempstack to stack
+stack.push(eleToBePushedAtBottom);
+while(tempStack.isEmpty()==false){
+    let ele = tempStack.pop();
+    stack.push(ele);
+}
+
+// stack.display();
 
 
 
+
+
+// valid parthanthesis
+// let str = "{({})}((())())"
+let str = "{({}()[])}"
+let paraStack = new Stack();
+let ans = false;
+for (let index = 0; index < str.length; index++) {
+    const element = str[index];
+    if(element === "{" || element === "[" || element === "("){
+        paraStack.push(element);
+    }
+    if(element === "}" || element === "]" || element === ")"){
+        if(paraStack.isEmpty()){
+            ans= false;
+            break;
+        }
+        else if(element === ")"){
+            if(paraStack.getTop() !== "("){
+                ans = false;
+                break;
+            }
+        }
+        else if(element === "]"){
+            if(paraStack.getTop() !== "["){
+                ans = false;
+                break;
+            }
+        }
+        else if(element === "}"){
+            if(paraStack.getTop() !== "{"){
+                ans = false;
+                break;
+            }
+        }
+        paraStack.pop();
+    }
+    if(paraStack.isEmpty()){
+        ans = true;
+    }
+}
+
+
+console.log(ans);
