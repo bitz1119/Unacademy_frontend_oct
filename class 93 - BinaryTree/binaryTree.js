bfc64354273743b4f980659be6ebd7d1ead346d5
Bittoo Aggarwal <bitz1119@gmail.com>
2022-04-06 20:40:36 +0530

    tree traversal

---
+++ class 93 - BinaryTree/binaryTree.js
@@ -0,0 +1,58 @@
+class node {
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree{
+    constructor(){
+        this.root = null;
+    }
+
+    addNode(data, ...path){
+        let newNode = new node(data);
+        if(path.length == 0){
+            this.root = newNode;
+        }
+        else{
+            let temp = this.root;
+            for(let i = 0; i < path.length-1; i++){
+                if(path[i] == 'l'){
+                    temp = temp.left;
+                }
+                else{
+                    temp = temp.right;
+                }
+            }
+            if(path[path.length-1] == "l"){
+                temp.left = newNode;
+            }
+            else{
+                temp.right = newNode;
+            }
+        }
+    }
+
+    preorder(parent=this.root){
+        // base case
+        if(parent == null){
+            return;
+        }
+        console.log(parent.data);
+        this.preorder(parent.left);
+        this.preorder(parent.right);
+    }
+
+}
+let bt = new BinaryTree();
+bt.addNode(10)
+bt.addNode(20,"l")
+bt.addNode(70,"r")
+bt.addNode(30,"l","l")
+bt.addNode(40,"l","r")
+bt.addNode(50,"r","l")
+bt.addNode(60,"r","r")
+
+bt.preorder();
